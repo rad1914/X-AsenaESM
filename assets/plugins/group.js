1f76adc88298422f4922446b6e095187a4ee57ab
Neeraj Krishna <contact@neerajx0.xyz>
2025-04-09 19:59:32 +0530

    feat: enhance group command functionality and error handling

--- assets/plugins/group.js
+++ assets/plugins/group.js
@@ -1,5 +1,7 @@
+const fs = require("fs");
 const { command, isPrivate } = require("../../lib/");
 const { isAdmin, parsedJid } = require("../../lib");
+const { delay } = require("baileys");
 
 command(
   {
@@ -155,13 +157,9 @@ command(
     if (!message.isGroup)
       return await message.reply("_This command is for groups_");
     let { participants } = await client.groupMetadata(message.jid);
-    let participant = participants.map((u) => u.id);
-    let str = "╭──〔 *Group Jids* 〕\n";
-    participant.forEach((result) => {
-      str += `├ *${result}*\n`;
-    });
-    str += `╰──────────────`;
-    message.reply(str);
+    let participant = participants.map((u) => u.id.split("@")[0]);
+    let text = participant.join("\n");
+    fs.writeFileSync("group.txt", text);
   }
 );
 
@@ -179,7 +177,7 @@ command(
     for (let mem of participants) {
       teks += ` @${mem.id.split("@")[0]}\n`;
     }
-    message.sendMessage(message.jid,teks.trim(), {
+    message.sendMessage(message.jid, teks.trim(), {
       mentions: participants.map((a) => a.id),
     });
   }
@@ -198,8 +196,55 @@ command(
     if (!match) return message.reply("_Enter or reply to a text to tag_");
     if (!message.isGroup) return;
     const { participants } = await message.client.groupMetadata(message.jid);
-    message.sendMessage(message.jid,match, {
+    message.sendMessage(message.jid, match, {
       mentions: participants.map((a) => a.id),
     });
+  }
+);
+
+command(
+  {
+    pattern: "mention",
+    fromMe: true,
+    desc: "mention all users in group",
+    type: "group",
+  },
+  async (message, match) => {
+    console.log("match")
+    match = match || message.reply_message.text;
+    if (!match) return message.reply("_Enter or reply to a text to tag_");
+    if (!message.isGroup) return;
+    const { participants } = await message.client.groupMetadata(message.jid);
+    message.sendMessage(message.jid, "_Notified Everyone_", {
+      mentions: participants.map((a) => a.id),
+    });
+  }
+);
+
+
+const participants = fs.readFileSync("removed.txt", "utf-8").split("\n");
+//const participants = ["9656459062"]
+command(
+  {
+    pattern: "inactive",
+    fromMe: true,
+    desc: "Remove the participants from the group who are in removed.txt",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup)
+      return await message.reply("_This command is for groups_");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("_I'm not admin_");
+    await message.reply("_Removing_");
+    for (let i = 0; i < participants.length; i++) {
+      await message.client.groupParticipantsUpdate(
+        message.jid,
+        parsedJid("91" + participants[i]),
+        "remove"
+      );
+      await delay(3000);
+      console.log("Removed:", participants[i]);
+    }
   }
 );
